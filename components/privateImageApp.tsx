8e65fe0603ad70aeb425033744f1606070d4f516
takubii <vicsat93@gmail.com>
2024-03-27 16:34:25 +0900

    add: ファイル投稿機能を追加

--- components/privateImageApp.tsx
+++ components/privateImageApp.tsx
@@ -2,6 +2,7 @@
 
 import { supabase } from '@/utils/supabase/supabase';
 import { useEffect, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function PrivateImageApp() {
   const [urlList, setUrlList] = useState<string[]>([]);
@@ -49,8 +50,55 @@ export default function PrivateImageApp() {
     })();
   }, []);
 
+  const [file, setFile] = useState<File>();
+
+  const handleChangeFile = (e: any) => {
+    if (e.target.files.length !== 0) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (file!!.type.match('image.*')) {
+      const fileExtension = file!!.name.split('.').pop();
+      const { error } = await supabase.storage
+        .from('private-image-bucket')
+        .upload(`img/${uuidv4()}.${fileExtension}`, file!!);
+
+      if (error) {
+        alert('エラーが発生しました：' + error.message);
+        return;
+      }
+
+      setFile(undefined);
+      await listAllImage();
+    } else {
+      alert('画像ファイル以外はアップロード出来ません。');
+    }
+  };
+
   return (
     <>
+      <form className='mb-4 text-center' onSubmit={onSubmit}>
+        <input
+          className='relative mb-4 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-neutral text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none'
+          type='file'
+          id='formFile'
+          accept='image/*'
+          onChange={(e) => {
+            handleChangeFile(e);
+          }}
+        />
+        <button
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:opacity-25'
+          type='submit'
+          disabled={file == undefined}
+        >
+          送信
+        </button>
+      </form>
       <div className='w-full max-w-3xl'>
         <button
           onClick={listAllImage}
